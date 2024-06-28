aa51c4e92192951f3603e9ae9d6806a4f9e4ca1b
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-28 18:20:28 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -14,13 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        for message in Message.query.order_by(Message.created_at.asc).all():
+            message_dict = [message.to_dict() for message in message.messages]
+            return make_response(jsonify(message_dict), 200)
+    elif request.method == "POST":
+         data = request.get_json()
+         body = data.get("body")
+         username = data.get("username")
+         
+         if not body or not username:
+             return make_response({"error": "Missing required fields"}, 400)
+         
+         message = Message(body=body, username=username)
+         db.session.add(message)
+         db.session.commit()
+         
+         return make_response(jsonify(message.to_dict()), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods =["DELETE", "PATCH"])
 def messages_by_id(id):
-    return ''
-
+    if request.method == "DELETE":
+        message = Message.query.get_or_404(id)
+        db.session.delete(message)
+        db.session.commit()
+        
+        return make_response({"message":"Message succefully deleted"})
+    elif request.method == "PATCH":
+         message = Message.query.get_or_404(id)
+         data = request.get_json()
+         body = data.get('body')
+         
+         if body:
+             message.body = body
+             db.session.commit()
+             
+         return make_response(jsonify(message.to_dict()), 200)    
+        
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+from sqlalchemy.sql import func
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +14,16 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body=db.Column(db.String(50), nullable=False)
+    username=db.Column(db.String())
+    created_at=db.Column(db.DateTime, server_default=func.now())
+    updated_at=db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
